15e4ae47db3cebc0488bd7a0ebf68b772e24665a
Dylan Baker <1901368@abertay.ac.uk>
2021-02-15 15:55:19 +0000

    Questions edits

--- view/questions.php
+++ view/questions.php
@@ -3,7 +3,24 @@ include("header.php");
 include("../model/api.php");
 session_start();
 if($_SESSION["logged-in"] === "yes"){
-    header("Location: login.php");
+    $questiontxt = getTests();
+    $test = json_decode($testtxt);
+    for ($i = 0, $iMax = count($test); $i < $iMax; $i++) {
+        echo "
+        <div class=\"container\">
+            <div class=\"list-group-item list-group-item-action bg-light text-dark \">
+                <div class=\"d-flex w-100 justify-content-between\">
+                    <h5 class=\"mb-1\"><strong>{$test[$i]-> test_name} </strong></h5>
+                    <h7>{$test[$i]-> test_subject} </h7>
+                </div>
+
+            </div>
+        </div>
+        <br>";
+    }
+
+
+
     // if (isset($_POST['question'])) {
     // $question = make_safe($_REQUEST['question']);
     // // escapes special characters in a string
@@ -40,43 +57,7 @@ if($_SESSION["logged-in"] === "yes"){
     // }
 }
 else{
-    echo ' 
-       <div class="st-xlarge st-text-grey">
-            Questions
-        </div>
-        <br>
-            
-            <div class="container">
-
-            <div class="list-group-item list-group-item-action bg-light text-dark ">
-                <div class="d-flex w-100 justify-content-between">
-                    <h5 class="mb-1">Log-In to ask a Question</h5>
-                </div>
-            
-            <button class="btn btn-primary st-black st-text-white st-border-black">
-                <a class="text-light" href="login.php">Log-In to an Existing Account</a>
-            </button>
-            
-            <button class="btn btn-primary st-black st-text-white st-border-black">
-                <a class="text-light" href="user_registration.php">Register a New User</a>
-            </button>
-            </div>
-        </div>
-        <br>';
+    echo "<script type='text/javascript'>window.top.location='https://mayar.abertay.ac.uk/~cmp311g20c05/production/view/login.php';</script>"; exit;
 }
-$questiontxt = getTests();
-$test = json_decode($testtxt);
-for ($i = 0, $iMax = count($test); $i < $iMax; $i++) {
-    echo "
-        <div class=\"container\">
-            <div class=\"list-group-item list-group-item-action bg-light text-dark \">
-                <div class=\"d-flex w-100 justify-content-between\">
-                    <h5 class=\"mb-1\"><strong>{$test[$i]-> test_name} </strong></h5>
-                    <h7>{$test[$i]-> test_subject} </h7>
-                </div>
 
-            </div>
-        </div>
-        <br>";
-    }
 include("footer.php");
